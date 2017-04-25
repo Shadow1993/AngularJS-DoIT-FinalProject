e911a8f347003b5f4fd2ef13349791c90fded6b5
Gojko <gojkodikic@yahoo.com>
2017-04-25 21:03:56 +0200

    Quick changes

--- development/js/app.js
+++ development/js/app.js
@@ -1,7 +1,15 @@
 'use strict';
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
 
-app.config(function($locationProvider) {
+app.config(function($locationProvider, $stateProvider) {
     $locationProvider.html5Mode(true);
+
+    var homeState = {
+        name: 'home',
+        url: '/',
+        templateUrl: './../templates/home.html'
+    };
+
+    $stateProvider.state(homeState);
 });
