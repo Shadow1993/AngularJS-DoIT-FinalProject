a131852c64d77f748b54774430fa8f6a61240f5a
Gojko <gojkodikic@yahoo.com>
2017-04-29 14:36:42 +0200

    Komitujem novu pitu

--- development/js/libs/artisan-dropdown.js
+++ development/js/libs/artisan-dropdown.js
@@ -1,17 +1,34 @@
 (function() {
     'use strict';
-    $(document).ready(
-        function() {
+    $(document)
+        .ready(function() {
+            function funcPos(event) {
+                var eTarget = event.target;
+                var myTarget = $(event.target).attr('data-artisan-dropdown');
+                if ($(myTarget).hasClass('artisan-dropdown-pos-left')) {
+                    $(myTarget).css({
+                        'top': eTarget.offsetTop + eTarget.getBoundingClientRect().top + eTarget.offsetHeight,
+                        'left': eTarget.offsetLeft + (eTarget.offsetWidth / 4)
+                    });
+                }
+            }
+            function funcHideAll() {
+                $('.artisan-dropdown-menu-dropped').addClass('artisan-dropdown-menu');
+                $('.artisan-dropdown-menu-dropped').removeClass('artisan-dropdown-menu-dropped');
+            }
             function funcDrop(event) {
                 var myTarget = $(event.target).attr('data-artisan-dropdown');
                 if ($(myTarget).hasClass('artisan-dropdown-menu')) {
                     $(myTarget).removeClass('artisan-dropdown-menu');
                     $(myTarget).addClass('artisan-dropdown-menu-dropped');
-                } else if ($(myTarget).hasClass('artisan-dropdown-menu-dropped')) {
-                    $(myTarget).removeClass('artisan-dropdown-menu-dropped');
-                    $(myTarget).addClass('artisan-dropdown-menu');
+                } else {
+                    funcHideAll();
                 }
             }
-            $(document).on('click', funcDrop);
+            $(window)
+                .on('resize', funcPos)
+                .on('resize', funcHideAll)
+                .on('click', funcPos)
+                .on('click', funcDrop);
         });
 }());
