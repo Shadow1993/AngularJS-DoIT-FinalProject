4b998c550fb24b2256e313add6d0ded1866a1880
Gojko Dikic <gojkodikic@yahoo.com>
2017-04-30 17:59:44 +0200

    Dusan dosao na pitu

--- development/js/app.js
+++ development/js/app.js
@@ -2,27 +2,50 @@
 
 var app = angular.module('app', ['ui.router']);
 
-app.config(function($locationProvider, $stateProvider) {
+app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
 
     var states = {
         home: {
             name: 'home',
-            url: '/',
+            abstract: 'true',
             views: {
-                '': {
+                '@': {
                     templateUrl: './../templates/home.html'
                 },
                 'navigation@home': {
                     templateUrl: './../templates/navigation.html',
                     controller: 'NavController',
                     controllerAs: 'nav'
+                },
+                'testMain@home': {
+                    templateUrl: './../templates/testMain.html'
                 }
             }
+        },
+        testMain: {
+            name: 'testMain',
+            abstract: true,
+            parent: 'home',
+            templateUrl: './../templates/testMain.html'
+        },
+        test: {
+            name: 'testMain.test',
+            url: '/test',
+            templateUrl: './../templates/test.html'
+        },
+        test2: {
+            name: 'testMain.test2',
+            url: '/test2',
+            templateUrl: './../templates/test2.html'
         }
     };
 
     $stateProvider
-        .state(states.home);
+        .state(states.home)
+        .state(states.testMain)
+        .state(states.test)
+        .state(states.test2);
 
     $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/test');
 });
