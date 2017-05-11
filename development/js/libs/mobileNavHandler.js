327519f0c86f59df2b978f69347e994616b8531a
Gojko <gojkodikic@yahoo.com>
2017-05-11 21:19:06 +0200

    Spammers fixed

--- development/js/libs/mobileNavHandler.js
+++ development/js/libs/mobileNavHandler.js
@@ -6,12 +6,27 @@
         $('#mobile-menu').removeClass('open');
     };
 
+    var funcAdd = function() {
+        $('.nav').addClass('mobileNavOpen');
+        $('#mobile-menu').addClass('open');
+    };
+
+    var spammers = function() {
+        if ($('#navDropMobile1').css('display') === 'block') {
+            funcAdd();
+        } else {
+            funcRemove();
+        }
+    };
+
     $(window)
         .on('resize', funcRemove)
         .on('click', function(event) {
             var funcToggle = function() {
                 $('.nav').toggleClass('mobileNavOpen');
                 $('#mobile-menu').toggleClass('open');
+                //Spam click fix
+                setTimeout(spammers, 900);
             };
             var testFor = {
                 parent: $(event.target).parents('#mobile-menu').attr('id') === 'mobile-menu',
