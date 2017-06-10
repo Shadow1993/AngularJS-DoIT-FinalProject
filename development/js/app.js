6c23b7451c04003d8267c2f9746d246927506549
Gojko <gojkodikic@yahoo.com>
2017-06-10 10:13:57 +0200

    validation

--- development/js/app.js
+++ development/js/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var app = angular.module('app', ['ui.router', 'satellizer', 'DataMock']);
+    var app = angular.module('app', ['ui.router', 'satellizer', 'DataMock', 'ngMessages']);
 
     app.config(function ($authProvider) {
 
